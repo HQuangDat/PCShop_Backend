d2910514f6187589b6260d7e91d7d3312a644386
HoQuangDat <hoquangdat123@gmail.com>
2025-10-17 18:51:15 +0700

    feat: include proper function for Product Interface

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -13,5 +13,6 @@ namespace PCShop_Backend.Controllers
         {
             _productService = productService;
         }
+
     }
 }
--- Service/IProductService.cs
+++ Service/IProductService.cs
@@ -1,6 +1,34 @@
+using PCShop_Backend.Models;
+using System.Runtime.CompilerServices;
+
 namespace PCShop_Backend.Service
 {
     public interface IProductService
     {
+        //Component 
+        Task<IEnumerable<Component>> getAllComponents();
+        Task<Component?> getComponentById(int id);
+        Task<IEnumerable<Component>> getComponentsByCategory(int CategoryId);
+        Task createComponent(Component newComponent);
+        Task updateComponent(int id, Component updatedComponent);
+        Task deleteComponent(int id);
+        //Component Specs
+        Task addComponentSpecs(int componentId, ComponentSpec spec);
+        Task<ComponentSpec?> getComponentSpecsById(int specId);
+        Task updateComponentSpecs(int specId, ComponentSpec updatedSpec);
+        Task deleteComponentSpecs(int specId);
+        Task<IEnumerable<ComponentSpec>> getAllComponentSpecs();
+        //Component Category
+        Task<ComponentCategory?> getComponentCategoryById(int categoryId);
+        Task addComponentCategory(ComponentCategory newCategory);
+        Task deleteComponentCategory(int categoryId);
+        Task updateComponentCategory(int categoryId, ComponentCategory componentCategory);
+        Task<IEnumerable<ComponentCategory>> getAllComponentCategories();
+        //Pcbuild
+        Task<Pcbuild?> getPcbuildById(int buildId);
+        Task createPcbuild(Pcbuild pcBuild);
+        Task updatePcbuild(int buildId, Pcbuild updatedBuild);
+        Task deletePcbuild(int buildId);
+        Task<IEnumerable<Pcbuild>> getAllPcbuilds();
     }
 }
