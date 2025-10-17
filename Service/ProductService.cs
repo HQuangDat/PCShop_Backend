290c7d86c156eb17d4a4b595351ab6be5c885288
HoQuangDat <hoquangdat123@gmail.com>
2025-10-17 18:49:58 +0700

    Refactor: Organize ProductService methods by entity and operation

--- Service/ProductService.cs
+++ Service/ProductService.cs
@@ -1,4 +1,5 @@
 using PCShop_Backend.Data;
+using PCShop_Backend.Models;
 
 namespace PCShop_Backend.Service
 {
@@ -9,6 +10,98 @@ namespace PCShop_Backend.Service
         public ProductService(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        // Component
+        public Task createComponent(Component newComponent)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<Component>> getAllComponents()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Component?> getComponentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<Component>> getComponentsByCategory(int CategoryId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task updateComponent(int id, Component updatedComponent)
+        {
+            throw new NotImplementedException();
+        }
+        public Task deleteComponent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // ComponentCategory
+        public Task addComponentCategory(ComponentCategory newCategory)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<ComponentCategory>> getAllComponentCategories()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ComponentCategory?> getComponentCategoryById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task updateComponentCategory(int categoryId, ComponentCategory componentCategory)
+        {
+            throw new NotImplementedException();
+        }
+        public Task deleteComponentCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        // ComponentSpec
+        public Task addComponentSpecs(int componentId, ComponentSpec spec)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<ComponentSpec>> getAllComponentSpecs()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ComponentSpec?> getComponentSpecsById(int specId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task updateComponentSpecs(int specId, ComponentSpec updatedSpec)
+        {
+            throw new NotImplementedException();
+        }
+        public Task deleteComponentSpecs(int specId)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Pcbuild
+        public Task createPcbuild(Pcbuild pcBuild)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<Pcbuild>> getAllPcbuilds()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Pcbuild?> getPcbuildById(int buildId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task updatePcbuild(int buildId, Pcbuild updatedBuild)
+        {
+            throw new NotImplementedException();
+        }
+        public Task deletePcbuild(int buildId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
